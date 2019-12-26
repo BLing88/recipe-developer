d00f2684d7c75bd98434b25f39b2b46cc32afab2
Brandon Ling <brandonvling@gmail.com>
2019-12-26 15:57:15 -0500

    Create example recipe object

---
+++ src/static-recipe.js
@@ -0,0 +1,10 @@
+const testRecipe = {
+  title: "Test Recipe",
+  ingredients: [
+    { name: "apples", quantity: 1 },
+    { name: "chocolate", quantity: "3 oz" }
+  ],
+  instructions: ["Chop apples", "Melt chocolate"]
+};
+
+export { testRecipe };

1beae8900f277acd01bb4153aa0e52d731bbc53f
Brandon Ling <brandonvling@gmail.com>
2019-12-26 19:07:51 -0500

    Make quantities strings

--- src/static-recipe.js
+++ src/static-recipe.js
@@ -1,7 +1,7 @@
 const testRecipe = {
   title: "Test Recipe",
   ingredients: [
-    { name: "apples", quantity: 1 },
+    { name: "apples", quantity: "1" },
     { name: "chocolate", quantity: "3 oz" }
   ],
   instructions: ["Chop apples", "Melt chocolate"]

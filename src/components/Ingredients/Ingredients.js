8864a91f1d6b2dffbda1bc9d737c8ab4dfad0ef0
Brandon Ling <brandonvling@gmail.com>
2020-02-25 17:44:42 -0500

    Change ingredient object structure

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,13 +4,11 @@ import PropTypes from "prop-types";
 const Ingredients = ({ ingredients }) => (
   <div className="ingredients">
     <h2>Ingredients</h2>
-    <ol className="ingredients-list">
+    <ul className="ingredients-list">
       {ingredients.map(ingredient => (
-        <li key={ingredient.name}>
-          {ingredient.name} - {ingredient.quantity}
-        </li>
+        <li key={ingredient.id}>{ingredient.ingredient}</li>
       ))}
-    </ol>
+    </ul>
   </div>
 );
 Ingredients.propTypes = {

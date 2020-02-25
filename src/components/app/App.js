5e3b1e7974e0649b0ed72a6bc05188bc1a648a10
Brandon Ling <brandonvling@gmail.com>
2020-02-25 16:11:37 -0500

    Add app header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Recipe } from "../Recipe";
-import { EditRecipeForm } from "../EditRecipeForm";
+// import { EditRecipeForm } from "../EditRecipeForm";
 import { testRecipe } from "../../static-recipe";
 
 const App = () => {
+  // eslint-disable-next-line
   const [recipe, setRecipe] = useState(testRecipe);
-  const updateRecipeHandler = event => {
-    event.preventDefault();
-    // TODO
-    // make API call to update recipe
-    // and get updated recipe
-    // setRecipe(updatedRecipe)
-  };
+  // const updateRecipeHandler = event => {
+  //   event.preventDefault();
+  //   // TODO
+  //   // make API call to update recipe
+  //   // and get updated recipe
+  //   // setRecipe(updatedRecipe)
+  // };
   return (
     <div className="app">
+      <nav className="app-header">
+        <button>My recipes</button>
+        <button>Create recipe</button>
+        <button>My profile</button>
+      </nav>
       <Recipe recipe={recipe} />
-      <EditRecipeForm updateRecipeHandler={updateRecipeHandler} />
+      {/* <EditRecipeForm updateRecipeHandler={updateRecipeHandler} /> */}
     </div>
   );
 };

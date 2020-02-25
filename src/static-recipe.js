4a8a589235d60c57a3298d7a1ac2e2ab7b8b7a10
Brandon Ling <brandonvling@gmail.com>
2020-02-25 16:13:08 -0500

    Add ids

--- src/static-recipe.js
+++ src/static-recipe.js
@@ -1,12 +1,27 @@
 const testRecipe = {
-  name: "Test Recipe",
-  id: 1,
+  name: "Gimbap",
+  id: "testRecipe",
   ingredients: [
-    { name: "apples", quantity: "1" },
-    { name: "chocolate", quantity: "3 oz" }
+    { name: "apples", quantity: "1", id: 1 },
+    { name: "chocolate", quantity: "3 oz", id: 2 }
   ],
-  instructions: ["Chop apples", "Melt chocolate"],
-  notes: ["Needs salt"]
+  instructions: [
+    {
+      instruction:
+        "Trim cucumber ends and cut it in half lengthwise. Using a spoon, scrape out the seeds from each half. Cut each half lengthwise into three long strips to make 6 strips total. Place cucumber strips on a plate, sprinkle all over with salt, and toss well to coat. Set aside.",
+      id: 1
+    },
+    {
+      instruction:
+        "In a small bowl, beat eggs with a pinch of salt. Lightly grease a 10-inch nonstick skillet with vegetable oil and heat over medium heat until warmed but not sizzling hot. Add half the egg, swirling to cover entire bottom of pan and cook until egg has just set on top. Using a rubber spatula, carefully free the egg round and transfer to a work surface to cool. Lightly re-grease pan if needed and repeat with remaining egg. Wipe out skillet.",
+      id: 2
+    },
+    { instruction: "Slice egg lengthwise into thin strips. Set aside.", id: 3 }
+  ],
+  notes: [
+    { note: "Needs salt", id: 1 },
+    { note: "Maybe add vinegar", id: 2 }
+  ]
 };
 
 export { testRecipe };

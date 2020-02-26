f74b9473f9a60b66b9b945a81b959f42f246c63a
Brandon Ling <brandonvling@gmail.com>
2020-02-26 12:16:51 -0500

    Use buildTestIngredient utility

--- src/components/Ingredients/Ingredients.test.js
+++ src/components/Ingredients/Ingredients.test.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Ingredients } from "./Ingredients";
-import { testRecipe } from "../../static-recipe";
+// import { testRecipe } from "../../static-recipe";
 import { render } from "@testing-library/react";
 
-test("it lists every ingredient", () => {
-  const { getByText } = render(
-    <Ingredients ingredients={testRecipe.ingredients} />
-  );
+// import { buildTestIngredients } from "../../../test/utils/generate";
+import { buildTestIngredients } from "utils/generate";
 
-  testRecipe.ingredients.forEach(ingredient => {
-    expect(getByText(ingredient.name, { exact: false })).toBeInTheDocument();
-    expect(
-      getByText(ingredient.quantity, { exact: false })
-    ).toBeInTheDocument();
+test("it lists every ingredient", () => {
+  const testIngredients = buildTestIngredients();
+  const { getAllByText } = render(
+    <Ingredients ingredients={testIngredients} />
+  );
+  const ingredientSet = new Set(
+    testIngredients.map(ingredient => ingredient.ingredient)
+  );
+  Array.from(ingredientSet).forEach(ingredient => {
+    const queries = getAllByText(ingredient, { exact: false });
+    queries.forEach(ingredient => expect(ingredient).toBeInTheDocument());
   });
 });

34fc39171ae648cd801ffca4549fcce576c50c06
Brandon Ling <brandonvling@gmail.com>
2020-02-26 13:44:07 -0500

    Remove default overrides object

--- test/utils/generate.js
+++ test/utils/generate.js
@@ -4,9 +4,9 @@ export const buildArray = (length, fn) => {
   return Array.from({ length }, fn);
 };
 
-export const buildTestIngredients = (overrides = { number: 15 }) => {
+export const buildTestIngredients = overrides => {
   const { number } = overrides;
-  const randomInt = number || faker.random.number(number);
+  const randomInt = number || faker.random.number(15);
   let ingredients = [];
   for (let i = 0; i < randomInt; i++) {
     ingredients.push({
@@ -17,7 +17,7 @@ export const buildTestIngredients = (overrides = { number: 15 }) => {
   return ingredients;
 };
 
-export const buildTestNotes = (overrides = { number: 15 }) => {
+export const buildTestNotes = overrides => {
   const { number } = overrides;
   const randomInt = number || faker.random.number(number);
   let notes = [];
@@ -30,7 +30,7 @@ export const buildTestNotes = (overrides = { number: 15 }) => {
   return notes;
 };
 
-export const buildTestInstructions = (overrides = { number: 15 }) => {
+export const buildTestInstructions = overrides => {
   const { number } = overrides;
   const randomInt = faker.random.number(number);
   let instructions = [];

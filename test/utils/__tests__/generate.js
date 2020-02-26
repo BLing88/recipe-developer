9a0708c8d9bbc71acf9e92768d6c4ffee7866d5a
Brandon Ling <brandonvling@gmail.com>
2020-02-26 13:27:39 -0500

    Add buildTestIngredients test

--- test/utils/__tests__/generate.js
+++ test/utils/__tests__/generate.js
@@ -1,9 +1,28 @@
-import { buildArray } from "./generate";
+import { buildArray, buildTestIngredients } from "../generate";
 
-describe("buildArray should construct an array of given length from function", () => {
-  test("array of squared numbers", () => {
+describe("buildArray ", () => {
+  test("constructs array of squared numbers", () => {
     const squares = buildArray(10, (_, index) => index * index);
     expect(squares).toHaveLength(10);
-    expect(squares).toEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100]);
+    expect(squares).toEqual([0, 1, 4, 9, 16, 25, 36, 49, 64, 81]);
+  });
+
+  test("constructs array of objects with index as property", () => {
+    const objects = buildArray(15, (_, i) => ({
+      i,
+    }));
+
+    objects.forEach(object => expect(object).toHaveProperty("i"));
+  });
+});
+
+describe("buildTestIngredients", () => {
+  test("constructs array of ingredient objects", () => {
+    const ingredients = buildTestIngredients({ number: 10 });
+    expect(ingredients).toHaveLength(10);
+    ingredients.forEach(ingredient => {
+      expect(ingredient).toHaveProperty("ingredient");
+      expect(ingredient).toHaveProperty("id");
+    });
   });
 });

0508804e0709d7c32c129e1af4747f9ce3b70ccd
Brandon Ling <brandonvling@gmail.com>
2020-02-26 14:53:32 -0500

    Fix overrides implementation

--- test/utils/generate.js
+++ test/utils/generate.js
@@ -4,36 +4,38 @@ export const buildArray = (length, fn) => {
   return Array.from({ length }, fn);
 };
 
-export const buildTestIngredients = overrides => {
-  const { number } = overrides;
-  const numberOfIngredients = number || faker.random.number(15);
+export const buildTestIngredients = (overrides = {}) => {
+  const numberOfIngredients = overrides.number || faker.random.number(15);
   return buildArray(numberOfIngredients, () => ({
     ingredient: faker.commerce.productMaterial(),
     id: faker.random.uuid(),
+    ...overrides,
   }));
 };
 
-export const buildTestNotes = overrides => {
-  const { number } = overrides;
-  const numberOfNotes = number || faker.random.number(15);
+export const buildTestNotes = (overrides = {}) => {
+  const numberOfNotes = overrides.number || faker.random.number(15);
   return buildArray(numberOfNotes, () => ({
     note: faker.lorem.sentences(),
     id: faker.random.uuid(),
+    ...overrides,
   }));
 };
 
-export const buildTestInstructions = overrides => {
-  const { number } = overrides;
-  const numberOfInstructions = number || faker.random.number(15);
+export const buildTestInstructions = (overrides = {}) => {
+  const numberOfInstructions = overrides.number || faker.random.number(15);
   return buildArray(numberOfInstructions, () => ({
     instruction: faker.lorem.sentences(),
     id: faker.random.uuid(),
+    ...overrides,
   }));
 };
 
-export const buildTestRecipe = (overrides = {}) => ({
-  name: faker.productName(),
+export const buildTestRecipe = overrides => ({
+  name: faker.commerce.productName(),
   id: faker.random.uuid(),
   ingredients: buildTestIngredients(),
+  instructions: buildTestInstructions(),
+  notes: buildTestNotes(),
   ...overrides,
 });

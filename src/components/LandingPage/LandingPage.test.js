1680373e68c583a1e09ce6ce654705024ca91a28
Brandon Ling <brandonvling@gmail.com>
2020-02-27 14:57:02 -0500

    Rename mock functions

--- src/components/LandingPage/LandingPage.test.js
+++ src/components/LandingPage/LandingPage.test.js
@@ -4,30 +4,41 @@ import { render } from "@testing-library/react";
 import { LandingPage } from "./LandingPage";
 
 const renderLandingPage = () => {
-  const loginWithRedirect = jest.fn().mockName("loginWithRedirect");
-  const signup = jest.fn().mockName("signup");
+  const mockLoginWithRedirect = jest.fn().mockName("loginWithRedirect");
+  const mockSignup = jest.fn().mockName("signup");
   const queries = render(
-    <LandingPage loginWithRedirect={loginWithRedirect} signup={signup} />
+    <LandingPage
+      loginWithRedirect={mockLoginWithRedirect}
+      signup={mockSignup}
+    />
   );
-  return { loginWithRedirect, signup, ...queries };
+  return { mockLoginWithRedirect, mockSignup, ...queries };
 };
 
 describe("Landing page", () => {
   test("has a login button", () => {
-    const { loginWithRedirect, signup, getByText } = renderLandingPage();
+    const {
+      mockLoginWithRedirect,
+      mockSignup,
+      getByText,
+    } = renderLandingPage();
     const loginButton = getByText(/login/i);
     user.click(loginButton);
-    expect(loginWithRedirect).toHaveBeenCalledTimes(1);
-    expect(loginWithRedirect).toHaveBeenCalledWith(/*nothing*/);
-    expect(signup).not.toHaveBeenCalled();
+    expect(mockLoginWithRedirect).toHaveBeenCalledTimes(1);
+    expect(mockLoginWithRedirect).toHaveBeenCalledWith(/*nothing*/);
+    expect(mockSignup).not.toHaveBeenCalled();
   });
 
   test("has a sign up button", () => {
-    const { loginWithRedirect, signup, getByText } = renderLandingPage();
+    const {
+      mockLoginWithRedirect,
+      mockSignup,
+      getByText,
+    } = renderLandingPage();
     const signupButton = getByText(/sign up/i);
     user.click(signupButton);
-    expect(signup).toHaveBeenCalledTimes(1);
-    expect(signup).toHaveBeenCalledWith(/*nothing*/);
-    expect(loginWithRedirect).not.toHaveBeenCalled();
+    expect(mockSignup).toHaveBeenCalledTimes(1);
+    expect(mockSignup).toHaveBeenCalledWith(/*nothing*/);
+    expect(mockLoginWithRedirect).not.toHaveBeenCalled();
   });
 });

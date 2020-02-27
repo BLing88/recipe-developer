75cfb6967c498361d953ec34984eac637cc9f122
Brandon Ling <brandonvling@gmail.com>
2020-02-27 14:56:20 -0500

    Clean up first load test

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -5,31 +5,23 @@ import App from "./App";
 jest.mock("../../react-auth0-spa");
 import { useAuth0 as mockUseAuth0 } from "../../react-auth0-spa";
 
-test("shows landing page when first loaded", () => {
-  const mockLoginWithRedirect = jest.fn().mockName("loginWithRedirect");
+describe("App", () => {
+  test("shows landing page when first loaded", () => {
+    const mockLoginWithRedirect = jest.fn().mockName("loginWithRedirect");
 
-  mockUseAuth0.mockReturnValueOnce({
-    loading: true,
-    user: undefined,
-    isAuthenticated: undefined,
-    loginWithRedirect: mockLoginWithRedirect,
+    mockUseAuth0.mockReturnValueOnce({
+      loading: true,
+      user: undefined,
+      isAuthenticated: undefined,
+      loginWithRedirect: mockLoginWithRedirect,
+    });
+    const { getByText, queryByText } = render(<App />);
+
+    const login = getByText(/login/i);
+    const signup = getByText(/sign up/i);
+    const redirecting = queryByText(/redirecting/i);
+    expect(login).toBeInTheDocument();
+    expect(signup).toBeInTheDocument();
+    expect(redirecting).toBeNull();
   });
-  const { getByText, queryByText, rerender } = render(<App />);
-
-  expect(getByText(/Redirecting/i)).toBeInTheDocument();
-  expect(queryByText(/login/i)).toBeNull();
-  expect(queryByText(/sign up/i)).toBeNull();
-
-  mockUseAuth0.mockReturnValueOnce({
-    loading: false,
-    user: undefined,
-    isAuthenticated: undefined,
-    loginWithRedirect: mockLoginWithRedirect,
-  });
-  rerender(<App />);
-
-  const login = getByText(/login/i);
-  const signup = getByText(/sign up/i);
-  expect(login).toBeInTheDocument();
-  expect(signup).toBeInTheDocument();
 });

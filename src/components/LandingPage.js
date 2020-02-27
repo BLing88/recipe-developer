d751cf321a93894ff60b5acebb7d2cd4c9f091ad
Brandon Ling <brandonvling@gmail.com>
2020-02-27 13:16:13 -0500

    Create LandingPage

---
+++ src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const LandingPage = ({ loginWithRedirect, signup }) => {
+  return (
+    <main>
+      <h1>Recipe Developer</h1>
+      <button onClick={() => loginWithRedirect()}>Login</button>
+      <section>
+        Don&aps;t have an account?{" "}
+        <button onClick={() => signup()}>Sign up</button>
+      </section>
+    </main>
+  );
+};
+LandingPage.propTypes = {
+  loginWithRedirect: PropTypes.func.isRequired,
+  signup: PropTypes.func.isRequired,
+};
+
+export default LandingPage;
+export { LandingPage };

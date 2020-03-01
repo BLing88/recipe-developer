d31e9a9bf5dc7a4156e11381eae52fc068dd2a83
Brandon Ling <brandonvling@gmail.com>
2020-03-01 16:34:05 -0500

    Navbar only shows when authenticated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,20 +2,16 @@ import React from "react";
 import { useAuth0 } from "../../react-auth0-spa";
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { logout } = useAuth0();
 
   return (
     <nav>
-      {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log in</button>
-      ) : (
-        <span>
-          <button>My recipes</button>
-          <button>Create recipe</button>
-          <button>My profile</button>
-          <button onClick={() => logout()}>Log out</button>
-        </span>
-      )}
+      <span>
+        <button>My recipes</button>
+        <button>Create recipe</button>
+        <button>My profile</button>
+        <button onClick={() => logout()}>Log out</button>
+      </span>
     </nav>
   );
 };

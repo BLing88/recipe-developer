737ab510d30f57bc5634ab707b7f7e0d50a47603
Brandon Ling <brandonvling@gmail.com>
2020-03-02 12:57:11 -0500

    Remove unused imports from App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,23 +2,13 @@ import React from "react";
 import "./App.css";
 import { LandingPage } from "../LandingPage";
 import { AuthenticatedApp } from "../AuthenticatedApp";
-// import { EditRecipeForm } from "../EditRecipeForm";
-import { CreateRecipeForm } from "../CreateRecipeForm";
 
 import { useAuth0 } from "../../react-auth0-spa";
 
 const App = () => {
   const { loading, user, isAuthenticated, loginWithRedirect } = useAuth0();
-  // const updateRecipeHandler = event => {
-  //   event.preventDefault();
-  //   // TODO
-  //   // make API call to update recipe
-  //   // and get updated recipe
-  //   // setRecipe(updatedRecipe)
-  // };
 
   if (!user) {
-    // return <CreateRecipeForm />;
     return (
       <LandingPage
         loginWithRedirect={loginWithRedirect}

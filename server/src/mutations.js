11e22d66171d7445e057c9674311231b4d6e2f1a
Brandon Ling <brandonvling@gmail.com>
2020-03-05 18:15:18 -0500

    Add missing gql call

--- server/src/mutations.js
+++ server/src/mutations.js
@@ -1,25 +1,25 @@
 const { gql } = require("apollo-server-lambda");
 
-const CREATE_RECIPE = `
-    mutation createRecipe($recipeInput: RecipeInput!) {
-        createRecipe(recipeInput: $recipeInput) {
-            recipeName
-            recipeId
-            authorId
-            ingredients {
-                ingredient
-                ingredientId
-            }
-            instructions {
-                instruction
-                instructionId
-            }
-            notes {
-                note
-                noteId
-            }
-        }
+const CREATE_RECIPE = gql`
+  mutation createRecipe($recipeInput: RecipeInput!) {
+    createRecipe(recipeInput: $recipeInput) {
+      recipeName
+      recipeId
+      authorId
+      ingredients {
+        ingredient
+        ingredientId
+      }
+      instructions {
+        instruction
+        instructionId
+      }
+      notes {
+        note
+        noteId
+      }
     }
+  }
 `;
 
 module.exports = {

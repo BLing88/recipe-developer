115e9cef780e29169d4fc7f71e30f4d5568ed68f
Brandon Ling <brandonvling@gmail.com>
2020-03-08 20:24:26 -0400

    Update recipe test

--- webapp/src/components/Recipe/Recipe.test.js
+++ webapp/src/components/Recipe/Recipe.test.js
@@ -4,11 +4,25 @@ import { Recipe } from ".";
 import { buildTestRecipe } from "test/utils/generate";
 import { nameOfRecipe } from "utils/recipe";
 
-test("Given recipe it shows title, ingredients, instructions", () => {
-  const recipe = buildTestRecipe();
-  const { getByText } = render(<Recipe recipe={recipe} />);
+describe("Recipe", () => {
+  test("Given recipe it shows title, ingredients, instructions, and notes", () => {
+    const recipe = buildTestRecipe();
+    const { ingredients, instructions, notes } = recipe;
+    const { getAllByText, getByText } = render(<Recipe recipe={recipe} />);
 
-  expect(getByText(nameOfRecipe(recipe))).toBeInTheDocument();
-  expect(getByText("Ingredients")).toBeInTheDocument();
-  expect(getByText("Instructions")).toBeInTheDocument();
+    expect(getByText(nameOfRecipe(recipe))).toBeInTheDocument();
+    expect(getByText("Ingredients")).toBeInTheDocument();
+    expect(getByText("Instructions")).toBeInTheDocument();
+    expect(getByText("Notes")).toBeInTheDocument();
+
+    for (let ingredient of ingredients) {
+      expect(getAllByText(ingredient.ingredient)).not.toBeNull();
+    }
+    for (let instruction of instructions) {
+      expect(getAllByText(instruction.instruction)).not.toBeNull();
+    }
+    for (let note of notes) {
+      expect(getAllByText(note.note)).not.toBeNull();
+    }
+  });
 });

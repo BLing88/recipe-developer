e35844f9e51f3b679a7d0f270fb65ee0c326c4e8
Brandon Ling <brandonvling@gmail.com>
2020-03-09 11:07:00 -0400

    Clicking recipe name switches to editable input

--- webapp/src/components/Recipe/Recipe.js
+++ webapp/src/components/Recipe/Recipe.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useReducer } from "react";
 import PropTypes from "prop-types";
 import "./Recipe.css";
 import { Ingredients } from "../Ingredients";
 import { Instructions } from "../Instructions";
 import { Notes } from "../Notes";
+import { nameOfRecipe } from "../../utils/recipe";
 
-const Recipe = ({ recipe }) => {
+const EDITING_NAME = "EDITING_NAME";
+const UPDATE_RECIPE_NAME_INPUT = "UPDATE_RECIPE_NAME_INPUT";
+const initialState = recipe => {
+  return {
+    editName: false,
+    recipeName: nameOfRecipe(recipe),
+  };
+};
+const recipeReducer = (state, action) => {
+  switch (action.type) {
+    case EDITING_NAME:
+      return {
+        ...state,
+        editName: true,
+      };
+    case UPDATE_RECIPE_NAME_INPUT:
+      return {
+        ...state,
+        recipeName: action.recipeName,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+const Recipe = ({ recipe, updateHandler }) => {
   const { recipeName, ingredients, instructions, notes } = recipe;
+  const [state, dispatch] = useReducer(recipeReducer, recipe, initialState);
+
   return (
     <article className="recipe">
-      <h1 className="recipe-name">{recipeName}</h1>
+      {!state.editName ? (
+        <h1
+          onClick={() => dispatch({ type: EDITING_NAME })}
+          className="recipe-name"
+        >
+          {recipeName}
+        </h1>
+      ) : (
+        <>
+          <label htmlFor="recipe-name">Recipe name</label>
+          <input
+            id="recipe-name"
+            type="text"
+            value={state.recipeName}
+            onChange={e => {
+              e.preventDefault();
+              dispatch({
+                type: UPDATE_RECIPE_NAME_INPUT,
+                recipeName: e.target.value,
+              });
+            }}
+            required
+          />
+        </>
+      )}
       {/* <picture className="recipe-image">
         <img src={require("../../assets/gimbap-mobile.png")} alt="gimbap" />
       </picture> */}
--- webapp/src/components/Recipe/Recipe.test.js
+++ webapp/src/components/Recipe/Recipe.test.js
@@ -3,9 +3,18 @@ import { render } from "@testing-library/react";
 import { Recipe } from ".";
 import { buildTestRecipe } from "test/utils/generate";
 import { nameOfRecipe } from "utils/recipe";
+import testUser from "@testing-library/user-event";
+
+const renderRecipe = () => {
+  const recipe = buildTestRecipe();
+  return {
+    recipe,
+    ...render(<Recipe recipe={recipe} />),
+  };
+};
 
 describe("Recipe", () => {
-  test("Given recipe it shows title, ingredients, instructions, and notes", () => {
+  test("shows title, ingredients, instructions, and notes", () => {
     const recipe = buildTestRecipe();
     const { ingredients, instructions, notes } = recipe;
     const { getAllByText, getByText } = render(<Recipe recipe={recipe} />);
@@ -24,5 +33,14 @@ describe("Recipe", () => {
     for (let note of notes) {
       expect(getAllByText(note.note)).not.toBeNull();
     }
+  });
+
+  test("clicking recipe name allows user to edit it", () => {
+    const { getByText, getByLabelText, recipe } = renderRecipe();
+
+    testUser.click(getByText(nameOfRecipe(recipe)));
+    const recipeNameInput = getByLabelText(/recipe name/i);
+    expect(recipeNameInput).toBeInTheDocument();
+    expect(recipeNameInput).toHaveValue(nameOfRecipe(recipe));
   });
 });

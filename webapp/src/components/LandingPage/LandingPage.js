732ec8f766e6a9e32fb9d277b3df0e9ae89f3be0
Brandon Ling <brandonvling@gmail.com>
2020-03-11 14:57:20 -0400

    Center app title

--- webapp/src/components/LandingPage/LandingPage.js
+++ webapp/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./LandingPage.module.css";
+import styles from "./LandingPage.module.css";
 
 const LandingPage = ({ loginWithRedirect, signup }) => {
   return (
     <main className="landing-page">
-      <h1 className="app-name">Recipe Developer</h1>
+      <h1 className={styles.appTitle}>Recipe Developer</h1>
       <button className="login-btn" onClick={() => loginWithRedirect()}>
         Login
       </button>

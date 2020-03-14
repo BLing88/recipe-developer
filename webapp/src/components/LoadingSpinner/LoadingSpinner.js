08c98294939b8aff02cad23f7e6b2c9e4a7e92f8
Brandon Ling <brandonvling@gmail.com>
2020-03-14 16:56:12 -0400

    Add size prop to LoadingSpinner

--- webapp/src/components/LoadingSpinner/LoadingSpinner.js
+++ webapp/src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from "react";
 import styles from "./LoadingSpinner.module.css";
 
-const svgWidth = 150;
-const svgHeight = 150;
-const radius = 150 / 4;
 const { cos, sin, PI } = Math;
 const angularSpeed = 1000 / 25;
+const DEFAULT_SIZE = "DEFAULT";
+const SMALL_SIZE = "SMALL";
 
-const updateTheta = theta => (theta + (2 * PI) / angularSpeed) % (2 * PI);
-const xPos = theta => radius * cos(theta);
-const yPos = theta => radius * sin(theta);
-
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ size = DEFAULT_SIZE }) => {
   const [theta, setTheta] = useState(PI / 2);
+  let svgWidth, CENTER_RADIUS, DOT_RADIUS;
+  switch (size) {
+    case SMALL_SIZE:
+      svgWidth = 75;
+      CENTER_RADIUS = 5;
+      DOT_RADIUS = 3;
+      break;
+    default:
+      svgWidth = 150;
+      CENTER_RADIUS = 7;
+      DOT_RADIUS = 5;
+  }
+  const svgHeight = svgWidth;
+  const radius = svgWidth / 4;
+  const updateTheta = theta => (theta + (2 * PI) / angularSpeed) % (2 * PI);
+  const xPos = theta => radius * cos(theta);
+  const yPos = theta => radius * sin(theta);
 
   useEffect(() => {
     const interval = setInterval(() => setTheta(updateTheta), angularSpeed);
@@ -33,41 +45,31 @@ const LoadingSpinner = () => {
         className={styles.spinnerDot}
         cx={svgWidth / 2}
         cy={svgHeight / 2}
-        r={7}
-        fill="rgb(11, 162, 254)"
-        stroke="rgb(11, 162, 254)"
+        r={CENTER_RADIUS}
       />
       <circle
         className={styles.spinnerDot}
         cx={svgWidth / 2 - xPos(theta)}
         cy={svgHeight / 2 - yPos(theta)}
-        r={5}
-        fill="rgb(11, 162, 254)"
-        stroke="rgb(11, 162, 254)"
+        r={DOT_RADIUS}
       />
       <circle
         className={styles.spinnerDot}
         cx={svgWidth / 2 + xPos(theta)}
         cy={svgHeight / 2 + yPos(theta)}
-        r={5}
-        fill="rgb(11, 162, 254)"
-        stroke="rgb(11, 162, 254)"
+        r={DOT_RADIUS}
       />
       <circle
         className={styles.spinnerDot}
         cx={svgWidth / 2 - xPos(theta - PI / 2)}
         cy={svgHeight / 2 - yPos(theta - PI / 2)}
-        r={5}
-        fill="rgb(11, 162, 254)"
-        stroke="rgb(11, 162, 254)"
+        r={DOT_RADIUS}
       />
       <circle
         className={styles.spinnerDot}
         cx={svgWidth / 2 + xPos(theta - PI / 2)}
         cy={svgHeight / 2 + yPos(theta - PI / 2)}
-        r={5}
-        fill="rgb(11, 162, 254)"
-        stroke="rgb(11, 162, 254)"
+        r={DOT_RADIUS}
       />
     </svg>
   );

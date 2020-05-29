fb7b89fc53e0a74714cc2420696e5a9fb60a0e19
Brandon Ling <brandonvling@gmail.com>
2020-05-29 14:17:57 -0400

    Add recipe types

--- webapp/src/react-app-env.d.ts
+++ webapp/src/react-app-env.d.ts
@@ -1,1 +1,27 @@
 /// <reference types="react-scripts" />
+
+namespace Recipe {
+  interface Ingredient {
+    ingredient: string;
+    ingredientId: string;
+  }
+
+  interface Instruction {
+    instruction: string;
+    instructionId: string;
+  }
+
+  interface Note {
+    note: string;
+    noteId: string;
+  }
+
+  interface Recipe {
+    recipeName: string;
+    authorId: string;
+    recipeId: string;
+    ingredients: Ingredient[];
+    instructions: Instruction[];
+    notes: Note[];
+  }
+}
